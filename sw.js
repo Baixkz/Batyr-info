ee8092551bbfc49dc222230b29c780117a3343cc
Baixkz <50baykz@gmail.com>
2026-04-03 19:05:57 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,46 @@
+const CACHE_NAME = 'batyr-info-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Service Worker-ді орнату және файлдарды кэшке сақтау
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Кэш ашылды, файлдар сақталуда...');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Файлдарды сұрағанда алдымен кэштен іздеу, таппаса интернеттен алу
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response; // Кэште бар болса, соны береді
+        }
+        return fetch(event.request); // Жоқ болса, интернеттен алады
+      })
+  );
+});
+
+// Ескі кэшті тазалау
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
